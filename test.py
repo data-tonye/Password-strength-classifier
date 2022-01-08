b38167440e0de2fe93c40d3b35355727304fe7cf
Tamunotonye Harry <tamunotonye.harry@gmail.com>
2022-01-08 13:40:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,8 @@ from urllib.error import URLError
 
 
 def load_data():
-    ## "https://drive.google.com/uc?export=download&id=19XHbRlvcq2svrpUobxbtnIK8_pF95Ck9"
-    data = "https://raw.githubusercontent.com/data-tonye/Password-strength-classifier/main/data.csv"
+    data = "https://drive.google.com/uc?export=download&id=19XHbRlvcq2svrpUobxbtnIK8_pF95Ck9"
     df = pd.read_csv(data, error_bad_lines = False)
-    df = df.drop(columns = ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5',	'Unnamed: 6'])
     df.dropna(inplace=True)
     return df
 
