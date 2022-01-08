ca360d32e76d27c62eadf6aeb735067b919ddb19
Tamunotonye Harry <tamunotonye.harry@gmail.com>
2022-01-08 11:54:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from urllib.error import URLError
 
 
 def load_data():
-    data = "https://github.com/data-tonye/Password-strength-classifier/blob/main/data.csv"
+    data = "data.csv"
     df = pd.read_csv(data, error_bad_lines = False)
     df.dropna(inplace=True)
     return df
